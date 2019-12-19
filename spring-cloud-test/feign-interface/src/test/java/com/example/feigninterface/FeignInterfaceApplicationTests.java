4895c64ff49b4ddff21111ee4b4564f5cdb8be92
likun@taibrain.com <12345678>
2019-12-20 07:20:29 +0800

    commit lk

---
+++ spring-cloud-test/feign-interface/src/test/java/com/example/feigninterface/FeignInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feigninterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
